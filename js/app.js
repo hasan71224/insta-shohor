4d19953f615247b30eec486810840fb25fac4afa
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-03-11 00:02:28 +0600

    fix description content limitation from displayContent

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -101,7 +101,7 @@ const createPost = (post) => {
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
-
+                
                 <div class="post__content">${displayContent(post.description)}</div>
 
                 <div class="post__infos">
